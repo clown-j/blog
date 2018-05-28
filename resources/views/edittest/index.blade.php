3649fc9460e19625362192a3a05d038246bcb30b
jiaominlong <jiaominlong@outlook.com>
2018-05-28 21:20:28 +0800

    使用wangEditor插件进行图片上传，成功！

--- resources/views/edittest/index.blade.php
+++ resources/views/edittest/index.blade.php
@@ -7,10 +7,30 @@
     <title>请输入文章内容</title>
 </head>
 <body>
-    <form action="/upload" method="post" enctype="multipart/form-data"> 
+    {{-- <form action="/upload" method="post" enctype="multipart/form-data"> 
     {{ csrf_field() }}
     <input type="file" name="file_name">
     <input type="submit" value="上传">
-    </form>
+    </form> --}}
+
+
+    <div id="editText">
+        <p>欢迎使用wangEditor富文本编辑器</p>
+    </div>
+
+    <script type="text/javascript" src="js/vendor/wangEditor/wangEditor.min.js"></script>
+    <script type="text/javascript">
+        var E = window.wangEditor;
+        var editor = new E('#editText');
+
+        {{-- 定义token字段 --}}
+        editor.customConfig.uploadImgParams = {
+            _token: '{{ csrf_token() }}',
+        }
+        editor.customConfig.uploadImgServer = '/upload';
+        editor.customConfig.uploadFileName = "file_name";
+        editor.create()
+    </script>
+
 </body>
 </html>
