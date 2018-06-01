3556d84cce03b641dd5a9083f0272ac86418a95c
jiaominlong <jiaominlong@outlook.com>
2018-06-01 10:43:44 +0800

    增加富文本框

--- resources/views/web/page/add-posts.blade.php
+++ resources/views/web/page/add-posts.blade.php
@@ -20,6 +20,40 @@
                         <label for="exampleInputPassword1">描述：</label>
                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                     </div>
+
+
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">文章内容：</label>
+                        <div id="editText">
+                            <p>请录入内容！</p>
+                        </div>
+                        <script type="text/javascript" src="js/vendor/wangEditor/wangEditor.min.js"></script>
+                        <script type="text/javascript">
+                            var E = window.wangEditor;
+                            var editor = new E('#editText');
+                    
+                            {{-- 定义token字段 --}}
+                            editor.customConfig.uploadImgParams = {
+                                _token: '{{ csrf_token() }}',
+                            }
+                            editor.customConfig.uploadImgServer = '/upload-img';
+                            editor.customConfig.uploadFileName = "file_name";
+                            editor.create()
+                    
+                            document.getElementById('btn1').addEventListener('click', function (){
+                                // 获取富文本框的值，复制给隐藏的输入框
+                                document.getElementById('content').value = editor.txt.html();
+                                // 表单提交。
+                                document.getElementById('postlal').submit();
+                            })
+                        </script>        
+                    </div>
+                        
+                    <div class="form-group">
+                        <button type="button" class="btn btn-primary">添加文章</button>
+                        <button type="button" class="btn btn-danger">重置表单</button>
+                    </div>
+
                     <select class="form-control input-lg">
                         <option value="cheese">Cheese</option>
                         <option value="tomatoes">Tomatoes</option>
