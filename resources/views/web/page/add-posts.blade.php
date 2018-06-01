c4a0252bf07e8e38f80ba4c1c0bed3c582af680d
jiaominlong <jiaominlong@outlook.com>
2018-06-01 10:32:02 +0800

    增加文章录入页面

--- resources/views/web/page/add-posts.blade.php
+++ resources/views/web/page/add-posts.blade.php
@@ -1,37 +1,53 @@
 {{--  文章添加页  --}}
 @extends('web.master')
+@section('title')
+文章录入
+@endsection
 @section('content')
 <div class="main-content">
-        <div class="container-fluid">
-            <h3 class="page-title">Typography</h3>
-            <div class="panel panel-headline">
-                <div class="panel-body">
-                    <h1>Dashboard Heading 1</h1>
-                    <h2>Dashboard Heading 2</h2>
-                    <h3>Dashboard Heading 3</h3>
-                    <h4>Dashboard Heading 4</h4>
-                    <h5>Dashboard Heading 5</h5>
-                    <h6>Dashboard Heading 6</h6>
-                    <hr>
-                    <p>Appropriately benchmark web-enabled bandwidth and functionalized leadership skills. Conveniently syndicate global opportunities without interactive methods of empowerment. Collaboratively conceptualize user-centric e-tailers for visionary methodologies. Dramatically myocardinate. Phosfluorescently disintermediate unique resources whereas reliable mindshare. Competently optimize client-focused infrastructures vis-a-vis e-business human capital. Uniquely formulate sustainable benefits whereas functional results. Energistically myocardinate bleeding-edge e-business.</p>
-                    <hr>
-                    <p class="text-muted"><code>.text-muted</code> Convey meaning through color with a handful of emphasis utility classes.</p>
-                    <p class="text-primary"><code>.text-primary</code> Convey meaning through color with a handful of emphasis utility classes.</p>
-                    <p class="text-success"><code>.text-success</code> Convey meaning through color with a handful of emphasis utility classes.</p>
-                    <p class="text-info"><code>.text-info</code> Convey meaning through color with a handful of emphasis utility classes.</p>
-                    <p class="text-warning"><code>.text-warning</code> Convey meaning through color with a handful of emphasis utility classes.</p>
-                    <p class="text-danger"><code>.text-danger</code> Convey meaning through color with a handful of emphasis utility classes.</p>
-                    <hr>
-                    <p>Make a paragraph stand out by adding <code>.lead</code></p>
-                    <p class="lead">Objectively re-engineer maintainable total linkage after proactive intellectual capital. Dynamically evolve best-of-breed e-services for user-centric customer.</p>
-                    <hr>
-                    <div class="well">
-                        <p class="text-left"><code>.text-left</code> Left aligned text.</p>
-                        <p class="text-center"><code>.text-center</code> Center aligned text.</p>
-                        <p class="text-right"><code>.text-right</code> Right aligned text.</p>
-                    </div>
+    <div class="container-fluid">
+        <h3 class="page-title">录入文章</h3>
+        <div class="panel">
+                <div class="panel-heading">
+                    <h3 class="panel-title">文章内容</h3>
                 </div>
-            </div>
-        </div>
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">文章标题：</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">描述：</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <select class="form-control input-lg">
+                        <option value="cheese">Cheese</option>
+                        <option value="tomatoes">Tomatoes</option>
+                        <option value="mozarella">Mozzarella</option>
+                        <option value="mushrooms">Mushrooms</option>
+                        <option value="pepperoni">Pepperoni</option>
+                        <option value="onions">Onions</option>
+                    </select>
+                    <br>
+                    <select class="form-control">
+                        <option value="cheese">Cheese</option>
+                        <option value="tomatoes">Tomatoes</option>
+                        <option value="mozarella">Mozzarella</option>
+                        <option value="mushrooms">Mushrooms</option>
+                        <option value="pepperoni">Pepperoni</option>
+                        <option value="onions">Onions</option>
+                    </select>
+                    <br>
+                    <select class="form-control input-sm">
+                        <option value="cheese">Cheese</option>
+                        <option value="tomatoes">Tomatoes</option>
+                        <option value="mozarella">Mozzarella</option>
+                        <option value="mushrooms">Mushrooms</option>
+                        <option value="pepperoni">Pepperoni</option>
+                        <option value="onions">Onions</option>
+                    </select>
+                </div>
+        </div>    
     </div>
+</div>
 @endsection
