e41e4203d06d1e6bc9c789a7abb271e8ccb72a1e
jiaominlong <jiaominlong@outlook.com>
2018-06-01 11:13:59 +0800

    编辑上传文件控制器

--- app/Http/Controllers/component/UploadController.php
+++ app/Http/Controllers/component/UploadController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class UploadController extends Controller
 {
-    //
+    public function upload(Request $request){
+        $storage_path = 'upload/'.date('Ymd');
+        $real_path = $request->file('img')->store($storage_path);
+    }
 }
